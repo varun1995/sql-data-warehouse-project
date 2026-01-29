a0ce12c093c196a799dbdea546a4ab137b7bf899
Varunsingh.R.Sisodia <varunsisodia95@gmail.com>
2026-01-29 17:24:21 +0530

    Add files via upload

--- scripts/bronze/ddl_bronze.sql
+++ scripts/bronze/ddl_bronze.sql
@@ -1,63 +1,90 @@
--- source CRM
-IF OBJECT_ID ('bronze.crm_cust_info','U') IS NOT NULL
-	DROP TABLE bronze.crm_cust_info
+/*
+===============================================================================
+DDL Script: Create Bronze Tables
+===============================================================================
+Script Purpose:
+    This script creates tables in the 'bronze' schema, dropping existing tables 
+    if they already exist.
+	  Run this script to re-define the DDL structure of 'bronze' Tables
+===============================================================================
+*/
+
+IF OBJECT_ID('bronze.crm_cust_info', 'U') IS NOT NULL
+    DROP TABLE bronze.crm_cust_info;
+GO
+
 CREATE TABLE bronze.crm_cust_info (
-cst_id INT,
-cst_key NVARCHAR(50),
-cst_firstname NVARCHAR(60),
-cst_lastname NVARCHAR(50),
-cst_material_status NVARCHAR(50),
-cst_gndr NVARCHAR(50),
-cst_create_date DATE
+    cst_id              INT,
+    cst_key             NVARCHAR(50),
+    cst_firstname       NVARCHAR(50),
+    cst_lastname        NVARCHAR(50),
+    cst_marital_status  NVARCHAR(50),
+    cst_gndr            NVARCHAR(50),
+    cst_create_date     DATE
 );
+GO
 
-IF OBJECT_ID ('bronze.crm_prd_info','U') IS NOT NULL
-	DROP TABLE bronze.crm_prd_info
+IF OBJECT_ID('bronze.crm_prd_info', 'U') IS NOT NULL
+    DROP TABLE bronze.crm_prd_info;
+GO
+
 CREATE TABLE bronze.crm_prd_info (
-prd_id	INT,
-prd_key	NVARCHAR(50),
-prd_nm	NVARCHAR(50),
-prd_cost	INT,
-prd_line	NVARCHAR(50),
-prd_start_dt	DATE,
-prd_end_dt DATE
+    prd_id       INT,
+    prd_key      NVARCHAR(50),
+    prd_nm       NVARCHAR(50),
+    prd_cost     INT,
+    prd_line     NVARCHAR(50),
+    prd_start_dt DATETIME,
+    prd_end_dt   DATETIME
 );
+GO
 
-IF OBJECT_ID ('bronze.crm_sales_details','U') IS NOT NULL
-	DROP TABLE bronze.crm_sales_details
+IF OBJECT_ID('bronze.crm_sales_details', 'U') IS NOT NULL
+    DROP TABLE bronze.crm_sales_details;
+GO
+
 CREATE TABLE bronze.crm_sales_details (
-sls_ord_num	NVARCHAR(50),
-sls_prd_key	NVARCHAR(50),
-sls_cust_id	INT,
-sls_order_dt VARCHAR(50),	
-sls_ship_dt	VARCHAR(50),
-sls_due_dt	VARCHAR(50),
-sls_sales	INT,
-sls_quantity INT,
-sls_price INT
+    sls_ord_num  NVARCHAR(50),
+    sls_prd_key  NVARCHAR(50),
+    sls_cust_id  INT,
+    sls_order_dt INT,
+    sls_ship_dt  INT,
+    sls_due_dt   INT,
+    sls_sales    INT,
+    sls_quantity INT,
+    sls_price    INT
 );
+GO
 
--- source ERP
-IF OBJECT_ID ('bronze.erp_CUST_AZ12','U') IS NOT NULL
-	DROP TABLE bronze.erp_CUST_AZ12
-CREATE TABLE bronze.erp_CUST_AZ12(
-CID	NVARCHAR(50),
-BDATE DATE,	
-GEN NVARCHAR(50)
-);
+IF OBJECT_ID('bronze.erp_loc_a101', 'U') IS NOT NULL
+    DROP TABLE bronze.erp_loc_a101;
+GO
 
-IF OBJECT_ID ('bronze.erp_LOC_A101','U') IS NOT NULL
-	DROP TABLE bronze.erp_LOC_A101
-CREATE TABLE bronze.erp_LOC_A101(
-CID	NVARCHAR(50),
-CNTRY NVARCHAR(50)
+CREATE TABLE bronze.erp_loc_a101 (
+    cid    NVARCHAR(50),
+    cntry  NVARCHAR(50)
 );
+GO
 
-IF OBJECT_ID ('bronze.erp_PX_CAT_G1V2','U') IS NOT NULL
-	DROP TABLE bronze.erp_PX_CAT_G1V2
-CREATE TABLE bronze.erp_PX_CAT_G1V2(
-ID	NVARCHAR(50),
-CAT	NVARCHAR(50),
-SUBCAT	NVARCHAR(50),
-MAINTENANCE NVARCHAR(50)
+IF OBJECT_ID('bronze.erp_cust_az12', 'U') IS NOT NULL
+    DROP TABLE bronze.erp_cust_az12;
+GO
+
+CREATE TABLE bronze.erp_cust_az12 (
+    cid    NVARCHAR(50),
+    bdate  DATE,
+    gen    NVARCHAR(50)
 );
+GO
+
+IF OBJECT_ID('bronze.erp_px_cat_g1v2', 'U') IS NOT NULL
+    DROP TABLE bronze.erp_px_cat_g1v2;
+GO
+
+CREATE TABLE bronze.erp_px_cat_g1v2 (
+    id           NVARCHAR(50),
+    cat          NVARCHAR(50),
+    subcat       NVARCHAR(50),
+    maintenance  NVARCHAR(50)
+);
+GO
